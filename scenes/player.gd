5f7213a6f2f5e3c3b5ef8b78dbae945ed21fbd73
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-07-06 17:33:28 +1200

    Made the bullet shoot from the player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,18 +1,27 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var bullet = preload("res://scenes/bullet.tscn")
+var canShoot=true
 
 
 func _physics_process(delta):
 	movement()
 	move_and_slide()
 	look_at(get_global_mouse_position())
-	if(Input.is_action_just_pressed("shoot")):
-		print("bang!")
+	if(Input.is_action_pressed("shoot") && canShoot):
+		canShoot=false
+		$shootCooldown.start()
+		var newBullet = bullet.instantiate()
+		newBullet.global_position = global_position
+		get_tree().current_scene.add_child(newBullet)
+		
 
 
 func movement():
 	var direction = Input.get_vector("left", "right","up","down")
 	velocity = direction * SPEED
+
+
+func _on_shoot_cooldown_timeout():
+	canShoot=true
